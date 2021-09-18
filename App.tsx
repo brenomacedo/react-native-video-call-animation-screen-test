f6606ed6c65d8b803845ec905c9d279bb7f0293a
Breno <b88660501@gmail.com>
2021-09-17 23:32:22 -0300

    setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const App: React.FC = () => {
+
+  return (
+    <View style={{flex: 1}}>
+    </View>
+  );
+};
+
+export default App;
